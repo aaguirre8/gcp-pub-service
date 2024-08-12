0f678e1feae1691865c3a4338e4a73dabb5b7d1a
aaguirre8 <agrr.68@gmail.com>
2024-08-11 18:02:07 -0600

    add pub

--- src/api/controller.py
+++ src/api/controller.py
@@ -1,3 +1,6 @@
+import json
+
+import google.cloud.pubsub_v1 as pubsub
 from nest.core import Controller, Depends, Post
 
 from src.api.service import AnalyticsService
@@ -10,7 +13,7 @@ class AnalyticsController:
 
     @Post("/publish_analytics")
     async def publish_analytics(self):
-        payload = [{
+        request = {
             "data": {"": ""},
             "ties": True,
             "target_col": "Electric Vehicles Revenue",
@@ -18,5 +21,10 @@ class AnalyticsController:
             "user_id": "user1",
             "session_id": "session1",
             "external_data_id": "data1",
-        }]
-        return payload
+        }
+
+        publisher = pubsub.PublisherClient()
+        topic_path = "projects/ezml-dev-417920/topics/run-xicorr"
+        payload = json.dumps(request).encode("utf-8")
+        future = publisher.publish(topic_path, data=payload)
+        return {"message": f"Published message ID {future.result()}"}
--- src/api/service.py
+++ src/api/service.py
@@ -1,20 +1,17 @@
+import google.cloud.pubsub_v1 as pubsub
 
 
 class AnalyticsService:
 
-    def input_analytics(self):
-        return [
-            {
-                "data": [
-                    {
-                        "row1": "value1",
-                    }
-                ],
-                "ties": True,
-                "target_col": "Electric Vehicles Revenue",
-                "date_col": "Calendar Date",
-                "user_id": "user1",
-                "session_id": "session1",
-                "external_data_id": "data1"
-            },  
-        ]       
+    def input_analytics(self, payload):
+        with open("payload.json", "w") as f:
+            f.write(str(payload))
+        
+    
+    def publish_analytics(self):
+        publisher = pubsub.PublisherClient() 
+        topic_path = publisher.topic_path("ezml-dev-417920", "run-xicorr")
+        for payload in self.input_analytics():
+            publisher.publish(topic_path, data=payload)
+        return "Published analytics"
+
